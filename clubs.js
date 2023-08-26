705fb33616818c969bc19d9a4444f842b5dc5e12
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-26 16:32:57 -0300

    Update clubs.js

--- clubs.js
+++ clubs.js
@@ -1,20 +1,20 @@
 class Club {
     constructor(club) {
-      this.id = club.id;
-      this.name = club.name;
-      this.shortname = club.shortname;
-      this.tla = club.tla;
-      this.area_name = club.area_name;
-      this.area_id = club.area_id;
-      this.crest_url = club.crest_url;
-      this.address = club.address;
-      this.phone = club.phone;
-      this.website = club.website;
-      this.email = club.email;
-      this.founded = club.founded;
-      this.club_colors = club.club_colors;
-      this.venue = club.venue;
-      this.created_at = club.created_at;
+      this.id = 57;
+      this.name = Club Atletico Rosario Central;
+      this.shortname = Central;
+      this.tla = CEN;
+      this.area_name = Argenrtina;
+      this.area_id = 2072;
+      this.crest_url = https://es.wikipedia.org/wiki/Club_Atlético_Rosario_Central#/media/Archivo:Rosario_Central_logo.png;
+      this.address = 640 Génova St., S2000 Rosario, Santa Fe;
+      this.phone = +54 0810-122-1889;
+      this.website = https://www.rosariocentral.com/;
+      this.email = socios@rosariocentral.com;
+      this.founded = 1889;
+      this.club_colors = Yellow / Blue;
+      this.venue = Gigante de Arroyito / Lisandro de La Torre Stadium;
+      this.created_at = "2023-08-26T16:32:34Z;
     }
   }
   
