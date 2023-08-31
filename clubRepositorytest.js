2c5a600292f4028a055d1adfe04e42027d7cc6dc
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-31 15:44:46 -0300

    Update clubRepositorytest.js

--- clubRepositorytest.js
+++ clubRepositorytest.js
@@ -5,14 +5,14 @@ const Club = require('../../entity/club');
 
 const sequelize = new Sequelize('sqlite::memory:');
 const ClubModel = Lanus (sequelize);
-const repository = new ClubRepository(ClubModel);
+const repository = new ClubRepository(Lanus);
 
 const clubMock = ClubModel.build({
   name: 'Club Atletico Lanus',
   shortname: 'Lanus',
   tla: 'LAN',
   area_name: 'Argentina',
-  area_id: 68,
+  area_id: 2072,
   crest_url: 'https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_Lan%C3%BAs#/media/Archivo:Modificacion_flyer_para_Editat%C3%B3n_del_Centenario_del_Club_Atl%C3%A9tico_Lan%C3%BAs.png',
   address: '2007 Ramón Cabrero St., B1824 Lanús, State of Buenos Aires',
   phone: '+54 (011) 4357-9200',
@@ -24,26 +24,26 @@ const clubMock = ClubModel.build({
 });
 
 const clubMock2 = ClubModel.build({
-  name: 'Chelsea FC',
-  shortname: 'Chelsea',
-  tla: 'CHE',
-  area_name: 'England',
+  name: 'Club Atletico Banfield',
+  shortname: 'Banfield',
+  tla: 'BAN',
+  area_name: 'Argentina',
   area_id: 2072,
-  crest_url: 'https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg',
-  address: 'Fulham Road London SW6 1HS',
-  phone: '+44 (0871) 9841955',
-  website: 'http://www.chelseafc.com',
-  email: null,
-  founded: 1905,
-  club_colors: 'Royal Blue / White',
-  venue: 'Stamford Bridge',
+  crest_url: 'https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_Banfield#/media/Archivo:CA_Banfield_(2014).svg',
+  address: '902 Gral. Arenales St. , B1828AFB Banfield, State of Buenos Aires',
+  phone: '+54 (011) 4242-1717',
+  website: 'https://www.clubabanfield.org/',
+  email: 'socios@clubabanfield.com.ar',
+  founded: 1896,
+  club_colors: 'White / Green',
+  venue: 'Florencio Sola Stadium',
 });
 
 describe('Club Repository', () => {
   beforeAll(async () => {
     await ClubModel.sync();
-    await clubMock.save();
-    await clubMock2.save();
+    await clubMock.save(Lanus);
+    await clubMock2.save(Banfield);
   });
 
   test('Should return all clubs entities', async () => {
