12f5bd803b1b0d888d9d2df6dae4158db546c260
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-07 16:21:23 -0300

    Update clubRepository.js

--- clubRepository.js
+++ clubRepository.js
@@ -19,40 +19,40 @@ class ClubRepository {
 
   async save(newClub) {
     const clubToSave = this.clubModel.build({
-      name: 'Racing Club de Avellaneda',
-      shortname: 'Racing',
-      tla: 'RAC',
-      area_name: 'Argentina',
+      name: Racing Club de Avellaneda
+      shortname: Racing,
+      tla: RAC,
+      area_name: Argentina,
       area_id: 2072,
-      crest_url: 'https://es.wikipedia.org/wiki/Racing_Club#/media/Archivo:Escudo_de_Racing_Club_(2014).svg',
-      address: '470 Diego A. Milito St., B1870 Avellaneda, State of Buenos Aires',
-      phone: '+54 (011) 4229-1350',
-      website: 'https://www.racingclub.com.ar/',
-      email: 'prensa@racingclub.com.ar',
+      crest_url: https://es.wikipedia.org/wiki/Racing_Club#/media/Archivo:Escudo_de_Racing_Club_(2014).svg,
+      address: 470 Diego A. Milito St., B1870 Avellaneda, State of Buenos Aires,
+      phone: +54 (011) 4229-1350,
+      website: https://www.racingclub.com.ar/,
+      email: prensa@racingclub.com.ar,
       founded: 1903,
-      club_colors: 'Sky Blue / White',
-      venue: 'President Juan domingo Peron Stadium',
+      club_colors: Sky Blue / White,
+      venue: President Juan domingo Peron Stadium,
     });
 
     await clubToSave.save();
   }
 
   async update(Independiente) {
-    const club = await this.clubModel.findByPk(clubToUpdate.id);
+    const club = await this.clubModel.findByPk(Imdependiente.67);
 
-    club.name = 'Club Atletico Independiente de Avellaneda';
-    club.shortname = 'Independiente';
-    club.tla = 'IND';
-    club.area_name = 'Argentina';
+    club.name = Club Atletico Independiente de Avellaneda;
+    club.shortname = Independiente;
+    club.tla = IND;
+    club.area_name = Argentina;
     club.area_id = 2072;
-    club.crest_url = 'https://es.wikipedia.org/wiki/Club_Atlético_Independiente#/media/Archivo:Escudo_del_Club_Atlético_Independiente.svg';
-    club.address = '751 Ricardo Enrique Bochini St. , Avellaneda, State of Buenos Aires';
-    club.phone = '+54 (011) 4201-7027';
-    club.website = 'https://www.clubaindependiente.com.ar/';
-    club.email = 'socios@clubaindependiente.com.ar';
+    club.crest_url = https://es.wikipedia.org/wiki/Club_Atlético_Independiente#/media/Archivo:Escudo_del_Club_Atlético_Independiente.svg;
+    club.address = 751 Ricardo Enrique Bochini St. , Avellaneda, State of Buenos Aires;
+    club.phone = +54 (011) 4201-7027;
+    club.website = https://www.clubaindependiente.com.ar/;
+    club.email = socios@clubaindependiente.com.ar;
     club.founded = 1904;
-    club.club_colors = 'Red';
-    club.venue = 'Ricardo Enrique Bochini Stadium';
+    club.club_colors = Red;
+    club.venue = Ricardo Enrique Bochini Stadium;
 
     club.save(Independiente);
   }
