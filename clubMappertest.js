4c91c3e4f38d0e0f0eff1915905e02e8f6d77e1c
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-07 16:18:20 -0300

    Update clubMappertest.js

--- clubMappertest.js
+++ clubMappertest.js
@@ -5,20 +5,20 @@ describe('Mapper of club entity', () => {
   test('Should return a club mapped', () => {
     const clubMockEntity = new Club({
       id: 64,
-      name: 'Club Atletico Colon de Santa Fe',
-      shortname: 'Colon',
-      tla: 'COL',
-      area_name: 'Argentina',
+      name: Club Atletico Colon de Santa Fe,
+      shortname: Colon,
+      tla: COL,
+      area_name: Argentina,
       area_id: 2072,
-      crest_url: 'https://es.wikipedia.org/wiki/Club_Atlético_Colón#/media/Archivo:Escudo_del_C._A._Colón.png',
-      address: '3535 Juan José Paso Avenue, S3000DXT Santa Fe de la Vera Cruz, Santa Fe',
-      phone: '+54 (0342) 4598025',
-      website: 'https://www.clubcolon.com.ar',
-      email: 'info@clubcolon.com.ar',
+      crest_url: https://es.wikipedia.org/wiki/Club_Atlético_Colón#/media/Archivo:Escudo_del_C._A._Colón.png,
+      address: 3535 Juan José Paso Avenue, S3000DXT Santa Fe de la Vera Cruz, Santa Fe,
+      phone: +54 (0342) 4598025,
+      website: https://www.clubcolon.com.ar,
+      email: info@clubcolon.com.ar,
       founded: 1905,
-      club_colors: 'Red / Black',
-      venue: 'Brigadier General Estanislao López Stadiu',
-      created_at: '2023-08-27T15:41:00Z',
+      club_colors: Red / Black,
+      venue: Brigadier General Estanislao López Stadium,
+      created_at: 2023-08-27T15:41:00Z,
     });
 
     const clubMockMappedExpected = {
