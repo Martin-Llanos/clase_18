ba0766dec2f2c7f7cdda77d22f41d11d7eff857f
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-07 16:29:19 -0300

    Update clubRepositorytest.js

--- clubRepositorytest.js
+++ clubRepositorytest.js
@@ -8,35 +8,35 @@ const ClubModel = Lanus (sequelize);
 const repository = new ClubRepository(Lanus);
 
 const clubMock = ClubModel.build({
-  name: 'Club Atletico Lanus',
-  shortname: 'Lanus',
-  tla: 'LAN',
-  area_name: 'Argentina',
+  name: Club Atletico Lanus,
+  shortname: Lanus,
+  tla: LAN,
+  area_name: Argentina,
   area_id: 2072,
-  crest_url: 'https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_Lan%C3%BAs#/media/Archivo:Modificacion_flyer_para_Editat%C3%B3n_del_Centenario_del_Club_Atl%C3%A9tico_Lan%C3%BAs.png',
-  address: '2007 Ramón Cabrero St., B1824 Lanús, State of Buenos Aires',
-  phone: '+54 (011) 4357-9200',
-  website: 'https://www.clublanus.com/',
-  email: 'contacto@clublanus.com',
+  crest_url: https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_Lan%C3%BAs#/media/Archivo:Modificacion_flyer_para_Editat%C3%B3n_del_Centenario_del_Club_Atl%C3%A9tico_Lan%C3%BAs.png,
+  address: 2007 Ramón Cabrero St., B1824 Lanús, State of Buenos Aires,
+  phone: +54 (011) 4357-9200,
+  website: https://www.clublanus.com/,
+  email: contacto@clublanus.com,
   founded: 1915,
-  club_colors: 'Maroon',
-  venue: 'Ciudad de Lanus Stadium',
+  club_colors: Crimson,
+  venue: Ciudad de Lanus Stadium,
 });
 
 const clubMock2 = ClubModel.build({
-  name: 'Club Atletico Banfield',
-  shortname: 'Banfield',
-  tla: 'BAN',
-  area_name: 'Argentina',
+  name: Club Atletico Banfield,
+  shortname: Banfield,
+  tla: BAN,
+  area_name: Argentina,
   area_id: 2072,
-  crest_url: 'https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_Banfield#/media/Archivo:CA_Banfield_(2014).svg',
-  address: '902 Gral. Arenales St. , B1828AFB Banfield, State of Buenos Aires',
-  phone: '+54 (011) 4242-1717',
-  website: 'https://www.clubabanfield.org/',
-  email: 'socios@clubabanfield.com.ar',
+  crest_url: https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_Banfield#/media/Archivo:CA_Banfield_(2014).svg,
+  address: 902 Gral. Arenales St. , B1828AFB Banfield, State of Buenos Aires,
+  phone: +54 (011) 4242-1717,
+  website: https://www.clubabanfield.org/,
+  email: socios@clubabanfield.com.ar,
   founded: 1896,
-  club_colors: 'White / Green',
-  venue: 'Florencio Sola Stadium',
+  club_colors: White / Green,
+  venue: Florencio Sola Stadium,
 });
 
 describe('Club Repository', () => {
@@ -60,19 +60,19 @@ describe('Club Repository', () => {
 
   test('Should save a club', async () => {
     const clubToSaveMock = {
-      name: 'Arsenal Futbol Club de Sarandí',
-      shortname: 'Arsenal',
-      tla: 'ARS',
-      area_name: 'Argentina',
+      name: Arsenal Futbol Club de Sarandí,
+      shortname: Arsenal,
+      tla: ARS,
+      area_name: Argentina,
       area_id: 2072,
-      crest_url: 'https://en.wikipedia.org/wiki/Arsenal_de_Sarandí#/media/File:Arsenal_Sarandí_logo.svg',
-      address: '3660 Julio H Grondona St. , B1872 Sarandí, State of Buenos Aires, Argentina',
-      phone: '+54 (011) 4204-0755',
-      website: 'https://www.arsenalfc.com.ar/',
-      email: 'prensa@arsenalfc.com.ar',
+      crest_url: https://en.wikipedia.org/wiki/Arsenal_de_Sarandí#/media/File:Arsenal_Sarandí_logo.svg,
+      address: 3660 Julio H Grondona St. , B1872 Sarandí, State of Buenos Aires, Argentina,
+      phone: +54 (011) 4204-0755,
+      website: https://www.arsenalfc.com.ar/,
+      email: prensa@arsenalfc.com.ar,
       founded: 1957,
-      club_colors: 'Sky Blue / Red',
-      venue: 'Julio Humberto Grondona Stadium',
+      club_colors: Sky Blue / Red,
+      venue: Julio Humberto Grondona Stadium,
     };
 
     await repository.save(Arsenal);
